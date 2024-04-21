5d3d8a9a9bfd4906d3a63aefe7b46510532bd035
Gregory Pereverzev <boot762@gmail.com>
2024-04-21 18:51:57 +0300

    feat: add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,30 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CurrencyComponent } from './components/currency/currency.component';
+import { LoveComponent } from './components/love/love.component';
+import { OrderComponent } from './components/order/order.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    MainContentComponent,
+    ProductsComponent,
+    CurrencyComponent,
+    LoveComponent,
+    OrderComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cookies';
