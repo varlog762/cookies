cda8a1cc0a23ae512c941e7695c113639e0e0ef7
Gregory Pereverzev <boot762@gmail.com>
2024-04-24 01:49:11 +0300

    feat: add ProductsEffects

--- src/app/store/effects/products.effects.ts
+++ src/app/store/effects/products.effects.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { switchMap } from 'rxjs/internal/operators/switchMap';
+import { map } from 'rxjs/internal/operators/map';
+import { catchError } from 'rxjs/internal/operators/catchError';
+import { EMPTY } from 'rxjs/internal/observable/empty';
 
-
+import { ProductsActions } from '../actions/products.actions';
+import { FetchDataService } from '../../services/fetch-data.service';
 
 @Injectable()
 export class ProductsEffects {
+  products$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(ProductsActions.loadProducts),
+      switchMap(() => {
+        return this.fetchDataService
+          .getProducts('https://testologia.ru/cookies')
+          .pipe(
+            map(products => ProductsActions.loadProductsSuccess({ products }))
+          );
+      }),
+      catchError(() => EMPTY)
+    )
+  );
 
-
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private fetchDataService: FetchDataService
+  ) {}
 }
